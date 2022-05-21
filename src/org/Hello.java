e85ccf375be85e11d95f036b9a634d43934dc30f
jeevarajendren <rjeeva098@gmail.com>
2022-05-21 23:15:51 +0530

    wert

--- src/org/Hello.java
+++ src/org/Hello.java
@@ -4,8 +4,9 @@ public class Hello {
 public static void main (String[]args ) {
 	for (int i = 1; i <=10; i++) {
 		if(i==5) {
+			System.out.println(i);
 	 
-	
+	break;
 		}
 		
 	System.out.println(i);
